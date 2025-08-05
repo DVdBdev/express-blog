38a1ffbac2fbbe82d67b5817e5a39e2d071f4d85
dvdbdev <Dries.Van.Den.Brande@proton.me>
2025-08-05 13:56:15 +0000

    start journey.ejs

--- express-blog/interfaces.ts
+++ express-blog/interfaces.ts
@@ -1,11 +1,11 @@
 import { ObjectId } from "mongodb";
 
 export interface User {
-    _id?: ObjectId;
-    email: string;
-    username: string;
-    password?: string;
-    role: "ADMIN" | "USER";
+  _id?: ObjectId;
+  email: string;
+  username: string;
+  password?: string;
+  role: "ADMIN" | "USER";
 }
 
 export interface Journey {
@@ -20,4 +20,29 @@ export interface Journey {
   kudos: number;
   tags: string[];
   imageUrl: string;
+}
+
+export interface Blog {
+  _id: string;
+  title: string;
+  content: EditorJsOutput;
+  createdAt: string;
+  journeyId: string;
+  author: {
+    _id: string;
+    username: string;
+  };
+  imageUrl: string;
+  tags: string[];
+  kudos: number;
+}
+
+interface EditorJsOutput {
+  time: number;
+  blocks: {
+    id?: string;
+    type: string;
+    data: Record<string, any>;
+  }[];
+  version: string;
 }
