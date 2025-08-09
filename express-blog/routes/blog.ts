444db72a9e7ae5bf0a1fbc5a122fd5c324ccf2f4
dvdbdev <Dries.Van.Den.Brande@proton.me>
2025-08-09 11:54:37 +0000

    view blog page

--- express-blog/routes/blog.ts
+++ express-blog/routes/blog.ts
@@ -1,17 +1,24 @@
 import { Router } from "express";
+import path from "path";
+import fs from "fs";
 
 const blogRouter = Router();
-
-blogRouter.get("/", (req, res) => {
-    res.render("list");
-});
 
 blogRouter.get("/new", (req, res) => {
     res.render("new");
 });
 
 blogRouter.get("/:id", (req, res) => {
-    res.render("view");
+    const blogId = req.params.id;
+
+    const filePath = path.join(__dirname, "../public/data/blogs.json");
+    const rawData = fs.readFileSync(filePath, "utf-8");
+    const blogs = JSON.parse(rawData);
+    const blog = blogs.find((b: any) => String(b._id) === String(blogId));
+    if (!blog) {
+        return res.status(404).send("Blog not found");
+    }
+    res.render("view", { blog });
 });
 
 blogRouter.get("/:id/edit", (req, res) => {
